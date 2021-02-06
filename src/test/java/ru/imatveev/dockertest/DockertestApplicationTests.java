44b4db40183fb27c70c4eae931bfd1f2150e5c22
vanadiiii <vanadiiii42@gmail.com>
2021-02-06 19:29:47 +0400

    Initial commit

---
+++ src/test/java/ru/imatveev/dockertest/DockertestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.imatveev.dockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
